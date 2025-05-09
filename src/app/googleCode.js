849734dd613023704a2ab1667b8d3907c118a5e9
葱 <offline2048@163.com>
2025-05-09 16:56:59 +0800

    开发环境不发送

--- src/app/googleCode.js
+++ src/app/googleCode.js
@@ -1,6 +1,11 @@
 import Script from 'next/script';
 
 export default function GoogleCode() {
+    console.log(process.env.NODE_ENV)
+    if (process.env.NODE_ENV !== 'production') {
+        return null; // 开发环境不加载 GA 脚本
+    }
+
     return (
         <>
             <Script
