9e567368216f18a0a142ffe78c0a682547fc3514
葱 <offline2048@163.com>
2025-05-27 10:00:31 +0800

    修改seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import styles from './style.css'
 
 
 export const metadata = {
-  description: "iFunny - A clean, customizable web launcher with drag-and-drop icons, folder grouping, and local saving. Organize your favorite sites into a minimal start page.", 
+  description: "iFunny - A clean, customizable web launcher with drag-and-drop icons, folder grouping, and local saving. Personalized bookmark page,Organize your favorite sites into a minimal start page.", 
   keywords:"iFunny,online tools,online games,web launcher, drag and drop dashboard, custom homepage, site organizer, bookmark launcher, online launcher, website collection, icon grid, personal dashboard, minimal start page",
   viewport:"width=device-width, initial-scale=1.0"  
 };
