a04c42d9974b3d06b9244b508dd8d8a7348d297e
葱 <offline2048@163.com>
2025-06-05 15:25:25 +0800

    添加siteMap和robot

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://iffunny.com/', // 你的网站地址
+    generateRobotsTxt: true,           // 自动生成 robots.txt
+    changefreq: 'monthly',
+    priority: 0.7,
+    sitemapSize: 5000,
+};
